6bc6154dbeec0032c1857ac0af5b34f7fa6c1f39
alozoghal <aloozoghal@gmail.com>
2025-12-29 09:14:43 +0330

    Update sw.jsتعغز

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
+const CACHE_NAME = 'alozoghal-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css'
+];
+
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-    // برای کارکرد صحیح دکمه نصب در مرورگر کروم
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
